1413b03f4242961709b8468076bdbb3a977656c2
papinaser <papinaser@gmail.com>
2019-04-18 01:06:18 +0430

    Updates

---
+++ precache-manifest.a8d6d4a6102c667690f2247a8a3ed9c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ee9b7ff41e9ddde3dae",
+    "url": "/reminderPro/static/js/runtime~main.8056a3b0.js"
+  },
+  {
+    "revision": "8e4979849c303a46a7a9",
+    "url": "/reminderPro/static/js/main.63478dcb.chunk.js"
+  },
+  {
+    "revision": "65689d834ac5bd5de7d0",
+    "url": "/reminderPro/static/js/2.b4768abe.chunk.js"
+  },
+  {
+    "revision": "8e4979849c303a46a7a9",
+    "url": "/reminderPro/static/css/main.dfb95350.chunk.css"
+  },
+  {
+    "revision": "cf928623de57c68191721ff487b567a8",
+    "url": "/reminderPro/index.html"
+  }
+];
